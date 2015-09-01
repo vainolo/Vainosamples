f74f7a9450c7b5b08167f035ccb07f8937845b9b
Arieh Bibliowicz <vainolo@gmail.com>
2015-09-01 21:42:07 +0300

    Added HTTP API Client example using Unirest

---
+++ JavaHTTPAPIClient/src/com/vainolo/examples/javahttpapiclient/JavaHTTPAPIClient.java
@@ -0,0 +1,31 @@
+package com.vainolo.examples.javahttpapiclient;
+
+import java.net.URI;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+
+public class JavaHTTPAPIClient {
+	public void getQuestionsUsingUnirest() throws Exception {
+		HttpResponse<JsonNode> response = Unirest.get("https://api.stackexchange.com/2.2/questions").
+				header("accept",  "application/json").
+				queryString("order","desc").
+				queryString("sort", "creation").
+				queryString("filter", "default").
+				queryString("site", "stackoverflow").
+				asJson();
+		System.out.println(response.getBody().getObject().toString(2));
+	}
+	
+	public static void main(String args[]) throws Exception {
+		JavaHTTPAPIClient client = new JavaHTTPAPIClient();
+		client.getQuestionsUsingUnirest();
+	}
+}
