4cb9b59407ff17e09dd18d180b83f599faeeed0a
Arieh Bibliowicz <arib@microsoft.com>
2020-10-04 19:32:38 +0300

    ASP.NET core background service sample

---
+++ CSharp/AspDotNetCoreBackgroundService/MyBackgroundService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspDotNetCoreBackgroundService
+{
+    public class MyBackgroundService : BackgroundService
+    {
+        private readonly ILogger<MyBackgroundService> _logger;
+
+        public MyBackgroundService(ILogger<MyBackgroundService> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation($"MyBackgroundService is executing.");
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
+            _logger.LogInformation("MyBackgroundService was interrupted.");
+        }
+    }
+}
