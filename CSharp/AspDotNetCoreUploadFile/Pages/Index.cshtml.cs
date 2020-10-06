609c819cbd9ddf8030c93ae6a289b491aeef5815
Arieh Bibliowicz <arib@microsoft.com>
2020-10-06 10:29:02 +0300

    File upload example

---
+++ CSharp/AspDotNetCoreUploadFile/Pages/Index.cshtml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Mime;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace AspDotNetCoreUploadFile.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IHostEnvironment _environment;
+
+        public string UploadedImage { get; private set; }
+
+        public IndexModel(ILogger<IndexModel> logger, IHostEnvironment environment)
+        {
+            _logger = logger;
+            _environment = environment;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task OnPostAsync(IFormFile uploadedFile)
+        {
+            if (uploadedFile == null || uploadedFile.Length == 0)
+            {
+                return;
+            }
+
+            _logger.LogInformation($"Uploading {uploadedFile.FileName}.");
+            string targetFileName = $"{_environment.ContentRootPath}/wwwroot/{uploadedFile.FileName}";
+
+            using (var stream = new FileStream(targetFileName, FileMode.Create))
+            {
+                await uploadedFile.CopyToAsync(stream);
+            }
+
+            if (uploadedFile.ContentType.IndexOf("image", StringComparison.OrdinalIgnoreCase) >= 0)
+            {
+                UploadedImage = $"/{uploadedFile.FileName}";
+            }
+        }
+    }
+}
---
+++ CSharp/AspDotNetCoreUploadFile/Startup.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AspDotNetCoreUploadFile
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
+        {
+            Configuration = configuration;
+            Env = env;
+        }
+
+        public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Env { get; private set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddRazorPages();
+#if DEBUG
+            if (Env.IsDevelopment())
+            {
+                builder.AddRazorRuntimeCompilation();
+            }
+#endif
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
