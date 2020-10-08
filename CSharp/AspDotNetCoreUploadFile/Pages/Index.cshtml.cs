e216df96c7f364cc9fb20f90f148bb0d91c3a6f2
Arieh Bibliowicz <arib@microsoft.com>
2020-10-08 06:19:42 +0300

    Finished file upload tutorial

--- CSharp/AspDotNetCoreUploadFile/Pages/Index.cshtml.cs
+++ CSharp/AspDotNetCoreUploadFile/Pages/Index.cshtml.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Mime;
+﻿using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.StaticFiles;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -17,8 +11,6 @@ namespace AspDotNetCoreUploadFile.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly IHostEnvironment _environment;
-
-        public string UploadedImage { get; private set; }
 
         public IndexModel(ILogger<IndexModel> logger, IHostEnvironment environment)
         {
@@ -38,16 +30,11 @@ namespace AspDotNetCoreUploadFile.Pages
             }
 
             _logger.LogInformation($"Uploading {uploadedFile.FileName}.");
-            string targetFileName = $"{_environment.ContentRootPath}/wwwroot/{uploadedFile.FileName}";
+            string targetFileName = $"{_environment.ContentRootPath}/{uploadedFile.FileName}";
 
             using (var stream = new FileStream(targetFileName, FileMode.Create))
             {
                 await uploadedFile.CopyToAsync(stream);
-            }
-
-            if (uploadedFile.ContentType.IndexOf("image", StringComparison.OrdinalIgnoreCase) >= 0)
-            {
-                UploadedImage = $"/{uploadedFile.FileName}";
             }
         }
     }
---
+++ CSharp/AspDotNetCoreUploadFile/Readme.md
@@ -0,0 +1,10 @@
+# Uploading a File in ASP.NET Core with Razor Pages
+
+Follow my tutorial [here](https://www.vainolo.com/2020/10/08/uploading-a-file-in-asp-net-core-with-razor-pages/) :-)
+
+## Instructions to Run the Project
+
+1. Navigate in the console to the project directory.
+1. Execute `dotnet restore`.
+1. Execute `dotnet run`.
+1. Enjoy life.
