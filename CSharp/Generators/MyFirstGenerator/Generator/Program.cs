2cbf152f77dd8fb6889236798aa389d956cbb82c
Arieh Bibliowicz <arib@microsoft.com>
2022-04-20 21:21:54 +0300

    First Code Generator

---
+++ CSharp/Generators/MyFirstGenerator/Generator/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Generator
+{
+    [Generator]
+    public class Generator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            // begin creating the source we'll inject into the users compilation
+            StringBuilder sourceBuilder = new StringBuilder(@"
+using System;
+namespace HelloWorldGenerated
+{
+    public static class HelloWorld
+    {
+        public static void SayHello() 
+        {
+            Console.WriteLine(""Hello from generated code!"");
+        }
+    }
+}");
+
+            // inject the created source into the users compilation
+            context.AddSource("HelloWorldGenerated.cs", SourceText.From(sourceBuilder.ToString(), Encoding.UTF8));
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+
+        }
+    }
+}
---
+++ CSharp/Generators/MyFirstGenerator/Program/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
